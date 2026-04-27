5a6490449702c99ec13fdbd5b957725d86223e42
styrian-oak <242596607+styrian-oak@users.noreply.github.com>
2026-04-27 22:46:43 +0200

    Add script that creates symlink required for OpenJDK

---
+++ setup_openjdk.zsh
@@ -0,0 +1,4 @@
+echo "\n<<< Starting OpenJDK Setup
+    Please enter root password so the symlink can be created. >>>\n"
+
+sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
